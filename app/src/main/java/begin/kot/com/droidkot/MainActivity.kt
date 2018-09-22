9ff67bbe3360b7a23ab597dcc5a74eeb906c5218
android200 <umarmanofpeace@gmail.com>
2018-09-22 01:15:25 +0100

    added forcastList Adapter and Pushing to Chapter 5

--- app/src/main/java/begin/kot/com/droidkot/MainActivity.kt
+++ app/src/main/java/begin/kot/com/droidkot/MainActivity.kt
@@ -2,21 +2,38 @@ package begin.kot.com.droidkot
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = listOf(
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+            "Sun 6/29 - Sunny - 20/7")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //val text = findViewById<TextView>(R.id.toast)
-        toast.text="Hello Kotlin Make it easy for mee"
+        val forcastList = findViewById<RecyclerView>(R.id.forcast_list)
+        forcastList.layoutManager = LinearLayoutManager(this)
+        forcastList.adapter = ForecastListAdapter(items)
+
+
+
+
         
 
     }
 
-    //fun display()=Toast.makeText(applicationContext,"message",Toast.LENGTH_SHORT).show()
+
 }
