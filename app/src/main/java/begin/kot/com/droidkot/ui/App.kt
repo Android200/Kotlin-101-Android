3ec357c58befa801ac686149ba3b5025c78abfa7
android200 <umarmanofpeace@gmail.com>
2018-09-26 13:30:12 +0100

    Chapter-16 Application Singleton and Delegated Properties

---
+++ app/src/main/java/begin/kot/com/droidkot/ui/App.kt
@@ -0,0 +1,15 @@
+package begin.kot.com.droidkot.ui
+
+import android.app.Application
+
+class App : Application(){
+
+    companion object {
+        var instance: App by DelegatesExt.notNullSingleValue()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
